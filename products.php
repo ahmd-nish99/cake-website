a109e558558ba6dbef326bfa57d3d68b0bb70115
ArzilRiyaj <arzilnishad@gmail.com>
2021-11-07 20:57:58 +0530

    latest update

--- products.php
+++ products.php
@@ -1,8 +1,15 @@
 <?php
 
 include_once "head.php";
+include_once "cake.php";
 
-?><?php
+$cake1 = new cake();
+
+      $q =$cake1->get_all_cake();
+?>
+
+
+<?php
 
 include_once "nav.php";
 
@@ -30,28 +37,39 @@ include_once "nav.php";
                     </div>
                     <div class="col-lg-5 col-md-5">
                         <div class="shop__option__right">
-                            <select>
-                                <option value="">Default sorting</option>
-                                <option value="">A to Z</option>
-                                <option value="">1 - 8</option>
-                                <option value="">Name</option>
-                            </select>
-                            <a href="#"><i class="fa fa-list"></i></a>
-                            <a href="#"><i class="fa fa-reorder"></i></a>
+                        <select name="cake_cat" class="form-control" id="cat_drop">
+
+<option value="1">  1</option>
+<option value="2">  2</option>
+<option value="3">  3</option>
+<option value="5">  4</option>
+
+
+
+
+
+</select>
+                           
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="row">
+
+<div  id="cakeliist">
+            <div class="row" >
+            <?php foreach($q as $item):  ?>
+
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="product__item">
-                        <div class="product__item__pic set-bg" style="background-image: url('assets/images/product-2.jpg');">
+                        <div class="product__item__pic set-bg">
+
+                        <img class="card-img-top" src="cake_image/<?=$item->cake_id ?>.jpg" alt="Card image cap">
                             <div class="product__label">
                                 <span>Cupcake</span>
                             </div>
                         </div>
                         <div class="product__item__text">
-                            <h6><a href="#">Dozen Cupcakes</a></h6>
+                            <h6><a href="#"><?=$item->cake_name ?></a></h6>
                             <div class="product__item__price">$32.00</div>
                             <div class="cart_add">
                                 <a href="#">Add to cart</a>
@@ -59,184 +77,11 @@ include_once "nav.php";
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" style="background-image: url('assets/images/product-2.jpg');">
-                            <div class="product__label">
-                                <span>Cupcake</span>
-                            </div>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">Cookies and Cream</a></h6>
-                            <div class="product__item__price">$30.00</div>
-                            <div class="cart_add">
-                                <a href="#">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" style="background-image: url('assets/images/product-2.jpg');">
-                            <div class="product__label">
-                                <span>Cupcake</span>
-                            </div>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">Gluten Free Mini Dozen</a></h6>
-                            <div class="product__item__price">$31.00</div>
-                            <div class="cart_add">
-                                <a href="#">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg"style="background-image: url('assets/images/product-2.jpg');">
-                            <div class="product__label">
-                                <span>Cupcake</span>
-                            </div>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">Cookie Dough</a></h6>
-                            <div class="product__item__price">$25.00</div>
-                            <div class="cart_add">
-                                <a href="#">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" style="background-image: url('assets/images/product-2.jpg');">
-                            <div class="product__label">
-                                <span>Cupcake</span>
-                            </div>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">Vanilla Salted Caramel</a></h6>
-                            <div class="product__item__price">$05.00</div>
-                            <div class="cart_add">
-                                <a href="#">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" style="background-image: url('assets/images/product-2.jpg');">
-                            <div class="product__label">
-                                <span>Cupcake</span>
-                            </div>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">German Chocolate</a></h6>
-                            <div class="product__item__price">$14.00</div>
-                            <div class="cart_add">
-                                <a href="#">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" style="background-image: url('assets/images/product-2.jpg');">
-                            <div class="product__label">
-                                <span>Cupcake</span>
-                            </div>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">Dulce De Leche</a></h6>
-                            <div class="product__item__price">$32.00</div>
-                            <div class="cart_add">
-                                <a href="#">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" style="background-image: url('assets/images/product-2.jpg');">
-                            <div class="product__label">
-                                <span>Cupcake</span>
-                            </div>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">Mississippi Mud</a></h6>
-                            <div class="product__item__price">$08.00</div>
-                            <div class="cart_add">
-                                <a href="#">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" style="background-image: url('assets/images/product-2.jpg');">
-                            <div class="product__label">
-                                <span>Cupcake</span>
-                            </div>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">VEGAN/GLUTEN FREE</a></h6>
-                            <div class="product__item__price">$98.85</div>
-                            <div class="cart_add">
-                                <a href="#">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" data-setbg="assets/images/product-10.jpg">
-                            <div class="product__label">
-                                <span>Cupcake</span>
-                            </div>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">SWEET CELTICS</a></h6>
-                            <div class="product__item__price">$5.77</div>
-                            <div class="cart_add">
-                                <a href="#">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" data-setbg="assets/images/product-11.jpg">
-                            <div class="product__label">
-                                <span>Cupcake</span>
-                            </div>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">SWEET AUTUMN LEAVES</a></h6>
-                            <div class="product__item__price">$26.41</div>
-                            <div class="cart_add">
-                                <a href="#">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" data-setbg="assets/images/product-12.jpg">
-                            <div class="product__label">
-                                <span>Cupcake</span>
-                            </div>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">PALE YELLOW SWEET</a></h6>
-                            <div class="product__item__price">$22.47</div>
-                            <div class="cart_add">
-                                <a href="#">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  
+                <?php endforeach ;  ?>
+             
             </div>
-        
+            </div>
         </div>
     </section>
     <!-- Shop Section End -->
@@ -251,3 +96,64 @@ include_once "nav.php";
 include_once "foot.php";
 
 ?>
+
+
+
+<script>
+
+
+
+$("#cat_drop").change(function()
+    {
+        var cat_id=$("#cat_drop").val();
+        console.log(cat_id);
+        
+
+        $.get("ajax.php?type=check_cake_cat",{cat:cat_id},function(data)
+        {
+
+          console.log(data);
+
+
+          $("#cakeliist").html("");
+            var txt='';
+            console.log(data);
+            var i_data = JSON.parse(data);
+            $.each(i_data,function(i,x)
+            {
+
+                txt=
+
+
+
+'<div class="col-lg-3 col-md-6 col-sm-6">'+
+                    '<div class="product__item">'+
+                        '<div class="product__item__pic set-bg" >'+
+                        
+                         ' <div class="product__label">'+
+                                '<span>Cupcake</span>'+
+                            '</div>'+
+                        '</div>'+
+                        '<div class="product__item__text">'+
+                            '<h6><a href="#">'+i_data[i].cake_name+'</a></h6>'
+                            '<div class="product__item__price">$32.00</div>'+
+                            '<div class="cart_add">'+
+                                '<a href="#">Add to cart</a>'+
+                            '</div>'+
+                       ' </div>'+
+                   ' </div>'+
+               ' </div>'+
+              
+                $("#cakeliist").append(txt);
+            
+
+            })
+
+
+
+        });
+    
+    });
+
+
+</script>
